17e55b52a01dfed2975eeae6d1fbe48135af162e
dlendnet <Alyssa.Meyerhofer11057@mail.ru>
2022-04-07 14:18:34 +0800

    deploy web : V0.01:init

---
+++ assets/welcome.html.9a21f6d1.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to dLend.Net",lang:"en-US",frontmatter:{index:1,title:"Welcome to dLend.Net",author:"dLend.Net",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`dLend Net : Decentralized Lend.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://dlend.net/welcome.html"}],["meta",{property:"og:title",content:"Welcome to dLend.Net"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"dLend.Net"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>dLend Net : Decentralized Lend.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"dLend Net",slug:"dlend-net",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why dLendNet?",slug:"why-dlendnet",children:[]},{level:2,title:"Tech Stack to Build dLend Net",slug:"tech-stack-to-build-dlend-net",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649312246e3,updatedTime:1649312246e3,contributors:[{name:"dlendnet",email:"Alyssa.Meyerhofer11057@mail.ru",commits:1}]},readingTime:{minutes:.44,words:132},filePathRelative:"welcome.md"};export{e as data};
